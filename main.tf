642cf5f7d3c6f0038776231f623e4bc2f12838b7
Mukesh Kumar <mukeshhkumar@gmail.com>
2026-08-12 15:33:46 +0530

    initial terraform lab setup

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "mkrg1" {
+  name     = "prod-rg"
+  location = "centralindia"
+  tags = {
+    environment = "lab"
+    managed_by  = "terraform"
+    project     = "github-gitlab-lab"
+
+  }
+}
